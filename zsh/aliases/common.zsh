b0db650aac5ee48afd7183a00e1898bb69638135
Peien Xie <peienxieit@gmail.com>
2021-05-31 01:52:54 +0800

    Feat(zsh): add zsh config

---
+++ zsh/aliases/common.zsh
@@ -0,0 +1,7 @@
+# Print each PATH entry on a separate line
+alias path="echo -e \"\${PATH//:/\\n}\""
+
+# Python
+alias py=python3
+alias python=python3
+
---
+++ zsh/exports/common.zsh
@@ -0,0 +1,9 @@
+# arm-none-eabi
+export PATH=/usr/lib/gcc/gcc-arm-none-eabi-4_9-2015q1/bin:$PATH
+
+# CppUTest
+export CPPUTEST_HOME=/opt/cpputest
+
+# Golang
+export GO_PATH=~/go
+export PATH=$PATH:$GO_PATH/bin
