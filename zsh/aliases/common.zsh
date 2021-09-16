50c9cbf402d6dbf53eb67277fb728abd36eeeb81
Peien Xie <peienxieit@gmail.com>
2021-09-16 23:13:11 +0800

    feat(zsh): add f alias with fdfind

--- zsh/aliases/common.zsh
+++ zsh/aliases/common.zsh
@@ -4,4 +4,8 @@ alias path="echo -e \"\${PATH//:/\\n}\""
 # Python
 alias py=python3
 alias python=python3
+alias ipy=ipython
+
+# fzf then cd the result has been selected
+alias f='cd $(fd --type directory | fzf)'
 
