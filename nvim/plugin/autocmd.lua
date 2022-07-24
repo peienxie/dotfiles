cbeecdeff42d6b4cedcacfc5d56231a0a3494046
Peien Xie <peienxieit@gmail.com>
2022-07-25 01:24:23 +0800

    feat: add autocmd.lua

---
+++ nvim/plugin/autocmd.lua
@@ -0,0 +1,19 @@
+vim.api.nvim_create_autocmd(
+    { "TextYankPost" }, {
+    command = "vim.highlight.on_yank { higroup='IncSearch', timeout=250 }",
+    group = vim.api.nvim_create_augroup("Hightlight_Yank", { clear = true }),
+})
+
+vim.api.nvim_create_autocmd(
+    { "WinEnter", "VimEnter" }, {
+    command = "call matchadd('Todo', 'TODO|FIXME|XXX', -1)",
+    group = vim.api.nvim_create_augroup("Hightlight_Todo", { clear = true }),
+})
+
+vim.api.nvim_create_autocmd(
+    { "BufEnter" }, {
+    pattern = "COMMIT_EDITMSG",
+    command = "startinsert",
+    group = vim.api.nvim_create_augroup("StartInsert_Commit", { clear = true }),
+})
+
