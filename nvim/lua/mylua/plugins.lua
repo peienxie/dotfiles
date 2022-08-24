edbc1188d90b5ca77eeda02c0cc044c828fafcd0
Peien Xie <peienxieit@gmail.com>
2022-08-25 02:25:30 +0800

    feat(vim): add vimwiki plugin

--- nvim/lua/mylua/plugins.lua
+++ nvim/lua/mylua/plugins.lua
@@ -86,4 +86,15 @@ return packer.startup(function(use)
 	use({ "akinsho/toggleterm.nvim" })
 
 	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install" })
+
+	use({
+		"vimwiki/vimwiki",
+		config = function()
+			vim.g.vimwiki_list = { { path = "~/vimwiki/", syntax = "markdown", ext = ".md" } }
+			-- automatically generate a level 1 header when creating a new wiki page.
+			vim.g.vimwiki_auto_header = 1
+			-- only set the filetype of markdown files inside a wiki directory
+			vim.g.vimwiki_global_ext = 0
+		end,
+	})
 end)
