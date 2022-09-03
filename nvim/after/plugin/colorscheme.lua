2f10b3d200023daaa42484efe95e3c1168bc3bd1
Peien Xie <peienxieit@gmail.com>
2022-09-03 18:04:20 +0800

    style(vim): update ayu colorscheme settings
    - override ayu palette for more visiable colors

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,4 +1,4 @@
--- dracula
+----- dracula ------
 --let g:colors_name = 'dracula'
 --vim.opt.background='dark'
 --vim.cmd([[ colorscheme dracula ]])
@@ -7,10 +7,21 @@
 --vim.cmd([[ highlight Character  NONE ]])
 --vim.cmd([[ highlight link Character DraculaYellow ]])
 
--- ayu
-vim.g.ayucolor = "dark"
-vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-vim.cmd([[ colorscheme ayu ]])
+----- ayu ------
+-- access the ayu#palette variable first then let plugin autoload this variable
+-- otherwise it is undefined variable
+vim.api.nvim_get_var("ayu#palette")
 
+-- not sure why this is not working, need use vim.cmd to assign variable
+--vim.g["ayu#palette"]["ui_panel_bg"]["dark"] = "#30353e"
+
+-- make float windows more visible
+vim.cmd('let g:ayu#palette["ui_panel_bg"]["dark"] = "#30353e"')
 -- make selected item more visible
-vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#30353e" })
+vim.cmd('let g:ayu#palette["ui_selection_active"]["dark"] = "#30353e"')
+-- Search and IncSearch
+vim.cmd('let g:ayu#palette["editor_findMatch_active"]["dark"] = "#897199"')
+vim.cmd('let g:ayu#palette["editor_findMatch_inactive"]["dark"] = "#5b5767"')
+
+vim.g.ayucolor = "dark"
+vim.cmd([[ colorscheme ayu ]])
