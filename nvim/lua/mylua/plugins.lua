be7269bbaf3fbb59349937b3ef5350937fa83ba3
Peien Xie <peienxieit@gmail.com>
2022-09-23 02:17:27 +0800

    fix(vim): change vimwiki list symbol to `x` and ` `

--- nvim/lua/mylua/plugins.lua
+++ nvim/lua/mylua/plugins.lua
@@ -113,6 +113,9 @@ return packer.startup(function(use)
 			vim.g.vimwiki_auto_header = 1
 			-- only set the filetype of markdown files inside a wiki directory
 			vim.g.vimwiki_global_ext = 0
+			-- the prettier markdown formatter will format the checkbox of completed task to lowercase x
+			-- if use default value ' .oOX' here, syntax highlight and <C-Space> keymap won't work with the formatter
+			vim.g.vimwiki_listsyms = " x"
 		end,
 	})
 end)
