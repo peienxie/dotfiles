76e2954ae0292193fc0d84323100adfcb00063cf
Peien Xie <peienxieit@gmail.com>
2022-10-08 16:54:44 +0800

    fix: set ts=4 for mixed indentation c filetype

---
+++ nvim/after/ftplugin/c.lua
@@ -0,0 +1,4 @@
+-- if any file has mixed indentation, vim-sleuth will set tabstop to '8'
+-- change it to 4 here which is I prefer for .c files
+-- https://github.com/tpope/vim-sleuth/commit/30096182b3d0c0f9e04609d016bef0b1bea7a374
+vim.g.sleuth_c_defaults = "ts=4"
