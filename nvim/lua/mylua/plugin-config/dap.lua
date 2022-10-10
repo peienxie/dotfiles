a2148b85dd1b146174b09ab295f4baaf43f2793d
Peien Xie <peienxieit@gmail.com>
2022-10-10 15:15:21 +0800

    style: update icon and highlight of DAP breakpoint

--- nvim/lua/mylua/plugin-config/dap.lua
+++ nvim/lua/mylua/plugin-config/dap.lua
@@ -73,6 +73,34 @@ dapui.setup({
 	},
 })
 
+vim.api.nvim_set_hl(0, "DapUIFloatBorder", { link = "Normal" })
+vim.api.nvim_set_hl(0, "DapBreakpoint", { fg = "#993939", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapLogPoint", { fg = "#61afef", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapStopped", { fg = "#98c379", bg = "#31353f" })
+
+vim.fn.sign_define(
+	"DapBreakpoint",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointCondition",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointRejected",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapLogPoint",
+	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
+-- hide dap-repl buffer
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = "dap-repl",
+	command = "set nobuflisted",
+	group = vim.api.nvim_create_augroup("NobuflistedDapRepl", { clear = true }),
 })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
