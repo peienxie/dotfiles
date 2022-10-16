42b76292ed1497c881a789b3ced08bdff1584657
Peien Xie <peienxieit@gmail.com>
2022-10-16 21:56:19 +0800

    feat: update jdtls config
    - run jdtls with java17
    - provide more specific settings for jdtls server

--- nvim/lua/mylua/lsp/servers/jdtls.lua
+++ nvim/lua/mylua/lsp/servers/jdtls.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+-- jdtls need at least java 17 or newer version.
+-- use java17 binary if JAVA17_HOME environment variable is set
+-- otherwise use default java binary.
+local java_bin = "java"
+local java17_home = os.getenv("JAVA17_HOME")
+if java17_home ~= "" and vim.fn.executable(java17_home .. "/bin/java") then
+	java_bin = java17_home .. "/bin/java"
+end
+
 -- all servers are installed by mason
 local jdtls_home = require("mason-registry.jdtls"):get_install_path()
 local java_debug_home = require("mason-registry.java-debug-adapter"):get_install_path()
@@ -7,6 +16,11 @@ local java_test_home = require("mason-registry.java-test"):get_install_path()
 
 local root_markers = { ".git", "mvnw", "gradlew" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
+if root_dir == "" then
+	vim.notify("can't find any project root for current java file")
+	return
+end
+
 local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
 local workspace_dir = os.getenv("HOME") .. "/.cache/jdtls/workspace/" .. project_name
 local platform = "linux" -- or "mac" "win"
@@ -19,7 +33,7 @@ M.config = {
 	-- The command that starts the language server
 	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
 	cmd = {
-		"java",
+		java_bin,
 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
 		"-Dosgi.bundles.defaultStartLevel=4",
 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
@@ -49,7 +63,56 @@ M.config = {
 	-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
 	-- for a list of options
 	settings = {
-		java = {},
+		java = {
+			eclipse = { downloadSources = true },
+			maven = { downloadSources = true },
+			implementationsCodeLens = { enabled = true },
+			referencesCodeLens = { enabled = true },
+			references = { includeDecompiledSources = true },
+			signatureHelp = { enabled = true },
+			format = { enabled = false },
+			completion = {
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+					"org.mockito.Mockito.*",
+				},
+			},
+			contentProvider = { preferred = "fernflower" },
+			sources = {
+				organizeImports = {
+					starThreshold = 9999,
+					staticStarThreshold = 9999,
+				},
+			},
+			codeGeneration = {
+				toString = {
+					template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+				},
+				useBlocks = true,
+			},
+			configuration = {
+				updateBuildConfiguration = "interactive",
+				runtimes = {
+					{
+						name = "JavaSE-1.8",
+						path = os.getenv("JAVA8_HOME"),
+					},
+					{
+						name = "JavaSE-11",
+						path = os.getenv("JAVA11_HOME"),
+					},
+					{
+						name = "JavaSE-17",
+						path = os.getenv("JAVA17_HOME"),
+					},
+				},
+			},
+		},
 	},
 
 	-- Language server `initializationOptions`
@@ -73,8 +136,8 @@ M.config = {
 		require("jdtls.dap").setup_dap_main_class_configs()
 
 		local nnoremap = require("mylua.utils.keymap").nnoremap
-		nnoremap("<leader>tr", jdtls.test_nearest_method)
-		nnoremap("<leader>tf", jdtls.test_class)
+		nnoremap("<leader>tr", jdtls.test_nearest_method, { buffer = bufnr })
+		nnoremap("<leader>tf", jdtls.test_class, { buffer = bufnr })
 	end,
 }
 
