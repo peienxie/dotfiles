3ed44ef6d5c995bf40bd860720c5fafa89d5ec13
Peien Xie <peienxieit@gmail.com>
2023-07-02 21:28:39 +0800

    feat: update keymaps and options settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.fileencodings = "utf-8,big5,cp950,gbk,cp936,iso-2022-jp,sjis,euc-jp,japan,euc-kr,ucs-bom,utf-bom,latin1,iso8859-1"
+
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+
+opt.cursorline = true
+opt.cursorlineopt = "number"
+
+opt.colorcolumn = "81"
+
+opt.list = true
+opt.listchars = { tab = "» ", eol = "↓", trail = "·" }
+
+opt.swapfile = false
