62109f5e74673cb999728d70f2110c01a0c95b76
Peien Xie <peienxieit@gmail.com>
2023-07-11 00:58:36 +0800

    feat: add harpoon

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -13,4 +13,34 @@ return {
       vim.g.undotree_WindowLayout = 2
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    -- stylua: ignore
+    keys = {
+      { "<leader>HH", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Menu" },
+      { "<leader>Hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Menu" },
+      { "<leader>Ha", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add File" },
+      { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "which_key_ignore" },
+      { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "which_key_ignore" },
+      { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "which_key_ignore" },
+      { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "which_key_ignore" },
+      { "<leader>5", function() require("harpoon.ui").nav_file(5) end, desc = "which_key_ignore" },
+      { "<leader>6", function() require("harpoon.ui").nav_file(6) end, desc = "which_key_ignore" },
+      { "<leader>7", function() require("harpoon.ui").nav_file(7) end, desc = "which_key_ignore" },
+      { "<leader>8", function() require("harpoon.ui").nav_file(8) end, desc = "which_key_ignore" },
+      { "<leader>9", function() require("harpoon.ui").nav_file(9) end, desc = "which_key_ignore" },
+      { "<leader>0", function() require("harpoon.ui").nav_file(10) end, desc = "which_key_ignore" },
+    },
+    opts = {},
+    dependencies = {
+      {
+        "folke/which-key.nvim",
+        opts = {
+          defaults = {
+            ["<leader>H"] = "+Harpoon",
+          },
+        },
+      },
+    },
+  },
 }
