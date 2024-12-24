543414c7f499aef5b72704d18856b0ad9201657a
Peien Xie <peienxieit@gmail.com>
2024-12-24 22:53:38 +0800

    fix: map <C-w> to delete word in Snacks.input

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -18,6 +18,19 @@ return {
           enabled = false,
         },
       },
+      input = {
+        -- reference: https://github.com/folke/snacks.nvim/discussions/376
+        win = {
+          keys = {
+            i_del_word = { "<C-w>", "delete_word", mode = "i" },
+          },
+          actions = {
+            delete_word = function()
+              return "<cmd>normal! diw<cr><right>"
+            end,
+          },
+        },
+      },
     },
   },
 }
