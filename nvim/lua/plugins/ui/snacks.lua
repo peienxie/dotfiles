b5ed1d92c7104115c03b776061acb5ebd8c678ae
Peien Xie <peienxieit@gmail.com>
2024-12-31 17:27:46 +0800

    feat: update sections of the dashboard

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -1,17 +1,16 @@
--- Terminal Mappings
-local function term_nav(dir)
-  ---@param self snacks.terminal
-  return function(self)
-    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
-      vim.cmd.wincmd(dir)
-    end)
-  end
-end
-
 return {
   {
     "snacks.nvim",
     opts = {
+      dashboard = {
+        sections = {
+          { section = "header" },
+          { section = "keys", padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 1, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 1, padding = 1 },
+          { section = "startup" },
+        },
+      },
       scroll = { enabled = false },
       ident = {
         animate = {
